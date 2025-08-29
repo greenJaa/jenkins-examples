b1b23c691f85b2e90a3eb0a4d08ef44a382b4bf0
greenJaa <34681632+greenJaa@users.noreply.github.com>
2025-08-29 22:57:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    return jsonify(success=True,message='From GUI')
+    return jsonify(success=True,message='Hello World')
 
 @app.route('/<name>')
 def hello(name):
